caf265e56270e653a53f3e9dfe97fe88b15e7345
Mehmet Hakel Çam <hakelcam@gmail.com>
2024-12-04 17:43:16 +0000

    calculated sediment params and bulk density for sand, silt and gravel

--- helms_parameters2.m
+++ helms_parameters2.m
@@ -8,17 +8,34 @@ h = W/Nx; % grid spacing based on image width and target grid size
 n_units = 9; % number of rock units contained in image
 [units,D,Nz] = ModelFromImage('section.tiff',n_units,W,Nx);
 % material properties for each rock unit (update based on your calibration)
+% calculate sediment parameters
+% set variables (values found online)
+
+rho_particle = 2650; % density of sand, gravel & silt
+rho_air = 1.225; % density of air
+sa_phi = 0.253; % sand porosity fraction
+gr_phi = 0.325; % gravel porosity fraction
+si_phi = 0.17; % silt porosity fraction
+sa_particle_kT = 8; % sand particle thermal conductivity
+gr_particle_kT = 5; % gravel particle thermal conductivity
+si_particle_kT = 3.5; % silt particle thermal conductivity
+air_kT = 0.025; % air thermal conductivity
+
+% calculate bulk density
+rho_sand = sa_phi * rho_air + (1-sa_phi) * rho_particle;
+rho_gravel = gr_phi * rho_air + (1-gr_phi) * rho_particle;
+rho_silt = si_phi * rho_air + (1-si_phi) * rho_particle;
 matprop = [
-% unit conductivity(kT) density(rho) heat capacity(Cp) heat production(Hr)
-1 3.678 2697.6 1000 4.172 %HE1
-2 1 2000 1000 1 %Gneiss
-3 1 2000 1000 1 %Sand
-4 3.218 2703.5 1000 5.575 %HE2
-5 1 2000 1000 1 %Gravel
-6 1 2000 1000 1 %Clay
-7 1 2000 1000 1 %Silt
-8 1 2000 1000 1 %Mud
-9 1e-6 1000 1000 0]; % air/water
+%   unit conductivity(kT)     density(rho)    heat capacity(Cp)      heat production(Hr)
+1       3.678                   2697.6                  1000                      4.172            %HE1
+2       1                       2000                    1000                      1                %Gneiss
+3       1                       rho_sand                1000                      1                %Sand
+4       3.218                   2703.5                  1000                      5.575            %HE2
+5       1                       rho_gravel              1000                      1                %Gravel
+6       1                       2000                    1000                      1                %Clay
+7       1                       rho_silt                1000                      1                %Silt
+8       1                       2000                    1000                      1                %Mud
+9       1e-6                    1000                    1000                      0];              % air/water
 % get coefficient fields based on spatial distribution of rock units from image
 % pay attention if any unit conversion is required!
 rho = reshape(matprop(units,3),Nz,Nx);
