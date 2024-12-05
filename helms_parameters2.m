77507d48d65b0465df24d1ae81f7321bc906fa54
Mehmet Hakel Çam <hakelcam@gmail.com>
2024-12-05 20:22:39 +0000

    updated params

--- helms_parameters2.m
+++ helms_parameters2.m
@@ -3,6 +3,8 @@
 % clear workspace
 clear all; close all; %clc;
 
+TEST = 'real'; % choose from 'calibration' and 'real'
+
 % load model setup from image, interpolate to target grid size
 W       = 16e3;     % domain width (must correspond to width of image) [m]
 Nx      = 200;      % target grid size z-direction
@@ -33,25 +35,40 @@ rho_silt   = si_phi * rho_air + (1-si_phi) * rho_particle;
 
 matprop = [
 % unit conductivity(kT) density(rho)  heat capacity(Cp)  heat production(Hr)
-   1	      3.678       2697.6	       1000	          4.172         %HE1
-   2	      1	          2000	           1000	          1             %Gneiss
-   3	      1	          rho_sand         1000	          1             %Sand
-   4	      3.218       2703.5	       1000	          5.575         %HE2
-   5	      1	          rho_gravel       1000	          0.6           %Gravel
-   6	      1	          2000	           1000	          2.75          %Clay
-   7	      1	          rho_silt         1000	          1.4           %Silt
-   8	      1	          2000	           1000	          3.5           %Mud
-   9	      1e-6        1000	           1000	          0];           % air/water
-         
-% get coefficient fields based on spatial distribution of rock units from image
-% pay attention if any unit conversion is required!
-%rho    = reshape(matprop(units,3),Nz,Nx);
-%Cp     = reshape(matprop(units,4),Nz,Nx);
-kT     = reshape(matprop(units,2),Nz,Nx);
-%Hr     = reshape(matprop(units,5),Nz,Nx);
+   1	      3.678       2697.6	       845	              4.172         %HE1
+   2	      1	          2000	           1000	              1             %Gneiss
+   3	      1	          rho_sand         830	              1             %Sand 1
+   4	      3.218       2703.5	       845	              5.575         %HE2
+   5	      1	          rho_gravel       1000	              0.6           %Gravel
+   6	      1	          2000	           1381	              2.75          %Clay
+   7	      1	          rho_silt         1000	              1.4           %Silt
+   8	      1	          2000	           2512	              3.5           %Mud
+   9	      1e-6        rho_air	       1000	              0];           % air/water
+
+switch TEST
+
+    case 'real'
+
+    % get coefficient fields based on spatial distribution of rock units from image
+    % pay attention if any unit conversion is required!
+    rho0    = reshape(matprop(units,3),Nz,Nx);
+    Cp      = reshape(matprop(units,4),Nz,Nx);
+    sigma   = reshape(matprop(units,2),Nz,Nx);
+    Hr      = reshape(matprop(units,5),Nz,Nx);
+
+    case 'calibration'
+    % calibration. set uniform parameters 
+    rho0 = 2400*ones(Nz,Nx);
+    Cp   = 1000*ones(Nz,Nx);
+    sigma  = ones(Nz,Nx);
+    Hr   = ones(Nz,Nx);
+
+end
+
+kappa = sigma ./ rho0 .* Cp;
 
 air = units == 9;
-geotherm = 30/1000;
+geotherm = 35/1000;
 
 %Test in constant coefficient unit value case (repeat for other variables)
 %rho = 2400*ones(Nz,Nx);
@@ -68,8 +85,8 @@ Ttop  = 5;                     % surface temperature
 Tbot  = Ttop + TGrad*(D/1000); % Temperature at the bottom of the domain using reference gradient
 
 
-rho0  = 1000;         % reference density [kg/m3]
-kT0   = 1e-7;         % heat diffusivity [m2/s]
+%rho0  = 1000;         % reference density [kg/m3]
+%kT0   = 1e-7;         % heat diffusivity [m2/s]
 cT    = 1e-9;         % kT T-dependence prefactor
 mT    = 2;            % kT T-dependence powerlaw
 g0    = 9.8;          % gravity [m/s2]
@@ -77,13 +94,9 @@ aT    = 1e-4;         % thermal expansivity
 ADVN  = 'WENO5';      % advection scheme ('UPW1', 'CFD2', 'UPW3', 'WENO5')
 
 yr    = 3600*24*365;  % seconds per year [s]
-tend  = 1e4*yr;       % stopping time [s]
-CFL   = 1/2;          % Time step limiter
-nop   = 10;           % output figure produced every 'nop' steps
-alpha = 0.99;         % iterative step size limiter
-beta  = 0.95;         % iterative lag parameter
-tol   = 1e-8;         % residual tolerance
-nup   = 100;          % update T, check residual every nup iterations
+tend  = 3e6*yr;       % stopping time [s]
+CFL   = 0.95;         % Time step limiter. Decreasing this increases the timestep. Less accuracy, quicker
+nop   = 100;          % output figure produced every 'nop' steps
 dTdto = 0;
 
 run('./helmsdale_take_2.m');
