f62f7618f88a775ff6053bca74e3cbf6e78aaebb
Mehmet Hakel Çam <78032311+Hakkelo89@users.noreply.github.com>
2024-12-13 00:31:21 +0000

    experimenting with helms_parameters2.m

--- helms_parameters2.m
+++ helms_parameters2.m
@@ -1,128 +1,119 @@
 %***** RUN 2D MODEL FROM IMAGE ***********************************
-% clear workspace
-clear all; close all; %clc;
-
-%Indicate whether this is a test run. Activating this option will enable the constant coefficient variant.
+% Indicate whether this is a test run. Activating this option will enable the constant coefficient variant.
 TEST = 'NO';
-NN = [100,200,400];
+NN = [100, 200, 400];
+
+% Define CFL condition parameter
+CFL = 0.5; % Choose an appropriate value for stability
 
 for nn = 1:3
 
-% Load the model setup from the image and interpolate it to match the desired target grid size.
-W               = 16e3;                                         % domain width (must correspond to width of image) [m]
-Nx              = NN(nn);                                       % target grid size z-direction
-h               = W/Nx;                                         % grid spacing based on image width and target grid size
-n_units         = 9;                                            % number of rock units contained in image
-[units,D,Nz]    = ModelFromImage('section.tiff',n_units,W,Nx);
+    % Load the model setup from the image and interpolate it to match the desired target grid size.
+    W               = 16e3;                                         % domain width (must correspond to width of image) [m]
+    Nx              = NN(nn);                                       % target grid size z-direction
+    h               = W / Nx;                                       % grid spacing based on image width and target grid size
+    n_units         = 9;                                            % number of rock units contained in image
+    [units, D, Nz]  = ModelFromImage('section.tiff', n_units, W, Nx);
 
+    % Compute sediment parameters
+    % Define variables (values sourced from online data)
+    rho_particle = 2650;          % density of sediments
+    rho_air      = 1.225;         % density of air
+    sa_phi       = 0.253;         % sand porosity proportion
+    gr_phi       = 0.325;         % gravel porosity proportion
+    si_phi       = 0.17;          % silt porosity proportion
 
-% Compute sediment parameters
-% Define variables (values sourced from online data)
+    sa_particle_kT = 8;           % sand particle thermal conductivity
+    gr_particle_kT = 5;           % gravel particle thermal conductivity
+    si_particle_kT = 3.5;         % silt particle thermal conductivity
+    air_kT         = 0.025;       % air thermal conductivity
 
-rho_particle = 2650;          % density of sediments
-rho_air      = 1.225;         % density of air
-sa_phi       = 0.253;         % sand porosity proportion
-gr_phi       = 0.325;         % gravel porosity proportion
-si_phi       = 0.17;          % silt porosity proportion
+    % Calculate the bulk density of the sediment, accounting for the sediment mass and air in the pores
+    rho_sand = (1 - sa_phi) * rho_particle + (sa_phi * rho_air);
+    rho_grav = (1 - gr_phi) * rho_particle + (gr_phi * rho_air);
+    rho_silt = (1 - si_phi) * rho_particle + (si_phi * rho_air);
 
-sa_particle_kT      = 8;        % sand particle thermal conductivity
-gr_particle_kT      = 5;        % gravel particle thermal conductivity
-si_particle_kT      = 3.5;      % silt particle thermal conductivity
-air_kT              = 0.025;    % air thermal conductivity
+    % Define material properties for each rock unit (update values as per calibration results)
+    matprop = [
+        % unit conductivity(kT) density(rho0) sp. heat capacity(Cp)   heat production(Hr)
+        1       3.678               2697.6      845                   4.172e-6              % HE1
+        2       2.467               2750        775                   2.9e-6                % Gneiss 
+        3       3.218               2703.5      845                   5.575e-6              % HE2
+        4       0.272               rho_sand    830                   1e-6                  % Sand
+        5       1.075               rho_grav    1000                  1e-6                  % Gravel
+        6       1.3                 2000        878                   1e-6                  % Clay (sea)
+        7       2.49                rho_silt    1000                  1e-6                  % Silt
+        8       0.61                2000        1510                  1e-6                  % Mud (Sea)
+        9       1e-6                rho_air     1012                  0                     % air/water
+    ];
 
-% Calculate the bulk density of the sediment, accounting for the sediment mass and air in the pores
-rho_sand = (1-sa_phi) * rho_particle + (sa_phi * rho_air);
-rho_grav = (1-gr_phi) * rho_particle + (gr_phi * rho_air);
-rho_silt = (1-si_phi) * rho_particle + (si_phi * rho_air);
+    % Generate coefficient fields based on the spatial distribution of rock units extracted from the image
+    switch TEST
+        case 'YES'
+            rho0        = 2400 * ones(Nz, Nx);
+            Cp          = 1000 * ones(Nz, Nx);
+            sigma       = 1000 * ones(Nz, Nx);
+            Hr          = ones(Nz, Nx); 
+            air         = units == 9;           % make a variable to represent the air
+            t_gradient  = 35 / 1000;            % Set a geothermal gradient of +30 degrees per 1000m of depth
+            geotherm    = [0, t_gradient]; 
+        case 'NO'
+            rho0        = reshape(matprop(units, 3), Nz, Nx); % Density
+            Cp          = reshape(matprop(units, 4), Nz, Nx); % Heat Capacity
+            sigma       = reshape(matprop(units, 2), Nz, Nx); % Conductivity
+            Hr          = reshape(matprop(units, 5), Nz, Nx); % Radiogenic Heating
+            air         = units == 9;                        % Represent air
+            t_gradient  = 35 / 1000;                         % Geothermal gradient
+            geotherm    = [0, t_gradient]; 
+    end
 
+    a = rho0 .* Cp;
+    kT0 = sigma * 1000 ./ a; 
 
-% Define material properties for each rock unit (update values as per calibration results)
-matprop = [
-% unit conductivity(kT) density(rho0) sp. heat capacity(Cp)   heat production(Hr)
-1       3.678               2697.6      845                   4.172e-6              %HE1
-2       2.467               2750        775                   5.7e-6                %Gneiss 
-3       3.218               2703.5      845                   5.575e-6              %HE2
-4       0.272               rho_sand    830                   1e-6                  %Sand
-5       1.075               rho_grav    1000                  1e-6                  %Gravel
-6       1.3                 2000        1381                  1e-6                  %Clay (sea)
-7       2.49                rho_silt    1000                  1e-6                  %Silt
-8       0.61                2000        2512                  1e-6                  %Mud (Sea)
-9       1e-6                rho_air     1012                  0];                   %air/water
+    % Set the remaining model parameters, then execute the model routine
+    Ttop    = 10;                       % Surface temperature
+    Tbot    = Ttop + D * geotherm(2);   % Bottom temperature using geotherm gradient
+    cT      = 1e-9;                     % kT T-dependence prefactor
+    mT      = 2;                        % kT T-dependence power law
+    g0      = 9.8;                      % Gravity [m/s2]
+    aT      = 1e-4;                     % Thermal expansivity [1/C]
+    yr      = 60 * 60 * 24 * 365.25;   % Seconds per year [s]
+    tend    = 1e6 * yr;                % Stopping time [s]
+    nop     = 100;                     % Output figure produced every 'nop' steps
 
-  
-% Generate coefficient fields based on the spatial distribution of rock units extracted from the image
-switch TEST
-    case 'YES'
-        rho0        = 2400*ones(Nz,Nx);
-        Cp          = 1000*ones(Nz,Nx);
-        sigma       = 1000*ones(Nz,Nx);
-        Hr          = ones(Nz, Nx); 
-        air         = units == 9;           %make a variable to represent the air: i.e. no heat production, constant temp
-        t_gradient  = 35/1000;              %Set a geothermal gradient of +30 degrees per 1000m of depth
-        geotherm    = [0, t_gradient]; 
-        
-        % pay attention if any unit conversion is required!
-    case 'NO'
-       rho0         = reshape(matprop(units,3),Nz,Nx);          %isolate the third column in the table of data to isolate rho (Density)
-       Cp           = reshape(matprop(units,4),Nz,Nx);          %isolate the fourth column in the table of data to isolate Cp (Heat Capacity)
-       sigma        = reshape(matprop(units,2),Nz,Nx);          %isolate the second column in the table of data to isolate kT (Conductivity)
-       Hr           = reshape(matprop(units,5),Nz,Nx);          %isolate the fifth column in the table of data to isolate Hr (Radiogenic Heating)
-       air          = units == 9;                               %make a variable to represent the air: i.e. no heat production, constant temp
-       t_gradient   = 35/1000;                                  %Set a geothermal gradient of +30 degrees per 1000m of depth
-       geotherm     = [0, t_gradient]; 
+    dTdto = 0;
+
+    % Run the main model script
+    run('./helmsdale_take_2.m');
 end
 
-a = rho0.*Cp;
-kT0 = sigma*1000./a; 
+%***** MAKEFIG FUNCTION (UPDATED) ********************************
 
-% Set the remaining model parameters, then execute the model routine
+function makefig(x, z, T, t, yr)
+    % Convert time to years
+    timeInYears = t / yr; 
 
-Ttop    = 10;                       % surface temperature
-Tbot    = Ttop + D*geotherm(2);     %Temperature at the bottom of the domain using geotherm gradient
-%rho0   = 1000;                     % reference density [kg/m3]
-%kT0    = 1e-6;                     % Set an initial value of heat diffusivity that is to be used [m2/s]
-cT      = 1e-9;                     % kT T-dependence prefactor
-mT      = 2;                        % kT T-dependence powerlaw
-g0      = 9.8;                      % gravity [m/s2]
-aT      = 1e-4;                     % thermal expansivity [1/C]
+    % Plot temperature
+    imagesc(x, z, T); axis equal; c = colorbar; hold on
+    contour(x, z, T, [100, 150, 150], 'k');
 
-yr      = 60*60*24*365.25;          % seconds per year [s]
-tend    = 1e6*yr;                   % stopping time [s]
-CFL     = 0.9;                      % Time step limiter 
-nop     = 100;                      % output figure produced every 'nop' steps
+    [C, h] = contour(x, z, T, [150, 150], 'r', 'LineWidth', 2); % 150°C contour in red
+    clabel(C, h, 'FontSize', 12, 'Color', 'r'); % Optional: Label the 150°C contour
 
-dTdto = 0;
+    [C, h] = contour(x, z, T, [100, 100], 'r', 'LineWidth', 2); % 100°C contour in red
+    clabel(C, h, 'FontSize', 12, 'Color', 'r'); % Optional: Label the 100°C contour
 
-run('./helmsdale_take_2.m');
+    xlabel('Horizontal Distance [m]', 'FontSize', 15)
+    ylabel('Depth [m]', 'FontSize', 15)
+    ylabel(c, 'C', 'FontSize', 15)
+    title('Temperature', 'FontSize', 18)
+
+    % Add time annotation
+    annotationText = sprintf('Time: %.2f years', timeInYears); % Format time in years
+    text(0.02, 0.98, annotationText, 'Units', 'normalized', ...
+        'FontSize', 14, 'Color', 'w', 'FontWeight', 'bold', ...
+        'HorizontalAlignment', 'left', 'VerticalAlignment', 'top', ...
+        'BackgroundColor', 'k', 'EdgeColor', 'w');
+
+    drawnow
 end
-
-%Plot numerical error
-Ex(nn)  = Errx;
-Dh(nn)  = h;
-
-%plot numerical error in x 
-figure(); 
-loglog(Dh,Ex,'ro','LineWidth',1.5,'MarkerSize',8); axis tight; box on; hold on
-loglog(Dh,Ex(1).*[1,1/2,1/4].^1,'k-','LineWidth',0.7)
-loglog(Dh,Ex(1).*[1,1/2,1/4].^2,'k-','LineWidth',0.9)
-loglog(Dh,Ex(1).*[1,1/2,1/4].^3,'k-','LineWidth',1.1)
-loglog(Dh,Ex(1).*[1,1/2,1/4].^4,'k-','LineWidth',1.3)
-loglog(Dh,Ex(1).*[1,1/2,1/4].^5,'k-','LineWidth',1.5)
-xlabel('Step size','FontSize',18)
-ylabel('Numerical error','FontSize',18)
-title('Numerical Convergence in Space','FontSize',20)
-
-Ez(nn)  = Errz;
-Dt(nn) = t;
-
-%plot numerical error in z
-figure(); 
-loglog(Dt,Ez,'ro','LineWidth',1.5,'MarkerSize',8); axis tight; box on; hold on
-loglog(Dt,Ez(1).*[1,1/2,1/4].^1,'k-','LineWidth',0.7)
-loglog(Dt,Ez(1).*[1,1/2,1/4].^2,'k-','LineWidth',0.9)
-loglog(Dt,Ez(1).*[1,1/2,1/4].^3,'k-','LineWidth',1.1)
-loglog(Dt,Ez(1).*[1,1/2,1/4].^4,'k-','LineWidth',1.3)
-loglog(Dt,Ez(1).*[1,1/2,1/4].^5,'k-','LineWidth',1.5)
-xlabel('Step size','FontSize',18)
-ylabel('Numerical error','FontSize',18)
-title('Numerical Convergence in Space','FontSize',20)
